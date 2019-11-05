4acfea191db2152d3d01a2ea1972d0476d97463e
James Liu <cyliustack@gmail.com>
2019-11-05 11:43:43 +0800

    first commit

---
+++ scull/build_and_run.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+## [Environment]
+## linux distro version: Fedora 25
+## linux kernel version: 4.10.10
+## gcc version: 6.3.1 
+## gcc-c++ version: 6.3.1
+
+## [Build]
+make
+
+## [Install]
+sudo cp scull.ko /lib/modules/$(uname -r)
+sudo depmod -a
+sudo rmmod scull
+sudo modprobe scull 
+dmesg
+ 
